5bb4164ef3bc594aab5fcedd456c5b648c839452
Eddy <3ddy.rakoto@gmail.com>
2021-06-12 21:23:43 +0200

    envoie migration

---
+++ migrations/Version20210612182102.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210612182102 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE date_start_service date_start_service VARCHAR(255) DEFAULT NULL, CHANGE date_naissance date_naissance VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE date_naissance date_naissance DATETIME DEFAULT NULL, CHANGE date_start_service date_start_service DATETIME NOT NULL');
+    }
+}
