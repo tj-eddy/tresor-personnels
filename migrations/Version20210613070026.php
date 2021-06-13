61c7b0149c91a2458ebfa4cca76e244da95d4843
Eddy <3ddy.rakoto@gmail.com>
2021-06-13 11:56:55 +0200

    fonction validation conge

---
+++ migrations/Version20210613070026.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210613070026 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE demande_conge (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, date_debut DATETIME DEFAULT NULL, lieu_jouissance VARCHAR(255) DEFAULT NULL, type_conge VARCHAR(255) DEFAULT NULL, motif VARCHAR(255) DEFAULT NULL, nom_interim VARCHAR(255) DEFAULT NULL, num_demande VARCHAR(255) DEFAULT NULL, INDEX IDX_D8061061A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE demande_conge ADD CONSTRAINT FK_D8061061A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE demande_conge');
+    }
+}
---
+++ src/Form/DemandeCongeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\DemandeConge;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DemandeCongeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date_debut')
+            ->add('lieu_jouissance')
+            ->add('type_conge')
+            ->add('motif')
+            ->add('nom_interim')
+            ->add('num_demande')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => DemandeConge::class,
+        ]);
+    }
+}
