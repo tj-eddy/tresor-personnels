68a7e6f5cbfd13416fb9959cd671ecdb0d248722
Eddy <3ddy.rakoto@gmail.com>
2021-06-20 20:04:57 +0200

    securisation route

--- public/index.php
+++ public/index.php
@@ -16,8 +16,6 @@ if ($_SERVER['APP_DEBUG']) {
 
     Debug::enable();
 }
-$controller = new DashboardController();
-
 $kernel   = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
 $request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
